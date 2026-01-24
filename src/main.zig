fabe55015999c36099eceb36f73c8d50e47c1ade
Luc Alexander <lucskyalex@gmail.com>
2026-01-24 12:53:26 +0100

    random hypergraph

--- src/main.zig
+++ src/main.zig
@@ -91,12 +91,56 @@ const State = struct {
 		};
 	}
 
+	pub fn parameterize_sub(self: *State, param: u64, target: u64) bool {
+		if (self.sub == .state){
+			if (self.sub.state == target){
+				self.sub = .{
+					.param = param
+				};
+				return true;
+			}
+		}
+		return false;
+	}
+
+	pub fn parameterize_id(self: *State, param: u64, target: u64) bool {
+		if (self.id == .state){
+			if (self.id.state == target){
+				self.id = .{
+					.param = param
+				};
+				return true;
+			}
+		}
+		return false;
+	}
+
 	pub fn clone(self: *State, _: *const std.mem.Allocator) State {
 		return self.*;
 	}
 
 	pub fn eql(a: State, b: State) bool {
-		return (a.id == b.id) and (a.sub == b.sub);
+		std.debug.assert(a.id == .state and b.id == .state and a.sub == .state and b.sub == .state);
+		return (a.id.state == b.id.state) and (a.sub.state == b.sub.state);
+	}
+	
+	pub fn show(self: *State) void {
+		switch (self.sub) {
+			.state => {
+				std.debug.print("({} ", .{self.sub.state});
+			},
+			.param => {
+				std.debug.print("([{}] ", .{self.sub.param});
+			}
+		}
+		switch(self.id) {
+			.state => {
+				std.debug.print("{}) ", .{self.id.state});
+			},
+			.param => {
+				std.debug.print("[{}]) ", .{self.id.param});
+			}
+		}
 	}
 };
 
@@ -106,12 +150,14 @@ const Rule = struct {
 	requires: Set(State),
 	consumes: Set(State),
 	introduces: Set(State),
+	degree: u64,
 
 	pub fn init(mem: *const std.mem.Allocator) Rule {
 		return Rule{
 			.requires = Set(State).init(mem),
 			.consumes = Set(State).init(mem),
-			.introduces = Set(State).init(mem)
+			.introduces = Set(State).init(mem),
+			.degree = 0
 		};
 	}
 
@@ -121,6 +167,50 @@ const Rule = struct {
 			.consumes = self.consumes.clone(mem),
 			.introduces = self.introduces.clone(mem)
 		};
+	}
+
+	pub fn parametric_on_sub(self: *Rule, param: u64) void {
+		var found = false;
+		for (self.requires.data.items) |*state| {
+			if (state.parameterize_sub(param, self.degree)){
+				found = true;
+			}
+		}
+		for (self.consumes.data.items) |*state| {
+			if (state.parameterize_sub(param, self.degree)){
+				found = true;
+			}
+		}
+		for (self.introduces.data.items) |*state| {
+			if (state.parameterize_sub(param, self.degree)){
+				found = true;
+			}
+		}
+		if (found){
+			self.degree += 1;
+		}
+	}
+	
+	pub fn parametric_on_id(self: *Rule, param: u64) void {
+		var found = false;
+		for (self.requires.data.items) |*state| {
+			if (state.parameterize_id(param, self.degree)){
+				found = true;
+			}
+		}
+		for (self.consumes.data.items) |*state| {
+			if (state.parameterize_id(param, self.degree)){
+				found = true;
+			}
+		}
+		for (self.introduces.data.items) |*state| {
+			if (state.parameterize_id(param, self.degree)){
+				found = true;
+			}
+		}
+		if (found){
+			self.degree += 1;
+		}
 	}
 	
 	pub fn apply(self: *Rule, param: std.AutoHashMap(u64, u64)) bool {
@@ -217,6 +307,22 @@ const Rule = struct {
 			and (Set(State).eql(a.consume, b.consume))
 			and (Set(State).eql(a.introduces, b.introduces));
 	}
+
+	pub fn show(self: *Rule) void {
+		std.debug.print("Rule of degree {}\nrequires: ", .{self.degree});
+		for (self.requires.data.items) |*state| {
+			state.show();
+		}
+		std.debug.print("\nconsumes: ", .{});
+		for (self.consumes.data.items) |*state| {
+			state.show();
+		}
+		std.debug.print("\nproduces: ", .{});
+		for (self.introduces.data.items) |*state| {
+			state.show();
+		}
+		std.debug.print("\n\n", .{});
+	}
 };
 
 const System = struct {
@@ -226,28 +332,58 @@ const System = struct {
 	rng: std.Random,
 
 	pub fn init(mem: *const std.mem.Allocator, rng: std.Random) System {
-		var sys = System{
+		const sys = System{
 			.rules = Set(Rule).init(mem),
 			.env = Environment.init(mem),
 			.mem = mem,
 			.rng = rng
 		};
 		const capabilities = rng.intRangeAtMost(u64, 8, 12);
-		const users = rng.intRangeAtMost(64, 2, 4);
-		var matrix = Buffer(bool).init(mem.*);
-		for (0..capabilities) |_| {
-			for (0..capabilities) |_| {
-				if (rng.intRangeAtMost(u64, 0, 4) == 0){
-					matrix.append(true)
-						catch unreachable;
+		const users = rng.intRangeAtMost(u64, 2, 4);
+		const rules = rng.intRangeAtMost(u64, 8, 16);
+		for (0..rules) |_| {
+			var rule = Rule.init(mem);
+			var n = rng.intRangeAtMost(u64, 0, 4);
+			for (0..n) |_| {
+				const cap = rng.intRangeAtMost(u64, 0, capabilities-1);
+				const use = rng.intRangeAtMost(u64, 0, users-1);
+				const state = State.init(cap, use);
+				rule.requires.put(state);
+			}
+			n = rng.intRangeAtMost(u64, 0, 4);
+			for (0..n) |_| {
+				const cap = rng.intRangeAtMost(u64, 0, capabilities-1);
+				const use = rng.intRangeAtMost(u64, 0, users-1);
+				const state = State.init(cap, use);
+				rule.consumes.put(state);
+			}
+			n = rng.intRangeAtMost(u64, 0, 4);
+			for (0..n) |_| {
+				const cap = rng.intRangeAtMost(u64, 0, capabilities-1);
+				const use = rng.intRangeAtMost(u64, 0, users-1);
+				const state = State.init(cap, use);
+				rule.introduces.put(state);
+			}
+			const parameters = rng.intRangeAtMost(u64, 0, 2);
+			for (0..parameters) |_| {
+				if (rng.intRangeAtMost(u64, 0, 1) == 0){
+					const param = rng.intRangeAtMost(u64, 0, capabilities-1);
+					rule.parametric_on_sub(param);
 				}
 				else{
-					matrix.append(false)
-						catch unreachable;
+					const param = rng.intRangeAtMost(u64, 0, users-1);
+					rule.parametric_on_id(param);
 				}
 			}
+			rule.show();
 		}
 		return sys;
+	}
+
+	pub fn show(self: *System) void {
+		for (self.rules.data.items) |*rule| {
+			rule.show();
+		}
 	}
 };
 
@@ -258,6 +394,7 @@ pub fn main() !void {
 	const mem = main_mem.allocator();
 	var rand = std.crypto.random;
 	var prng = std.Random.DefaultPrng.init(rand.int(u64));
-	_ = System.init(&mem, prng.random());
+	var sys = System.init(&mem, prng.random());
+	sys.show();
 
 }
